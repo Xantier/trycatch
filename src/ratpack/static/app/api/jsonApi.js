40b0753ff545e1e06eb155838a77b9c2087d2106
Jussi Hallila <jussi@hallila.com>
2016-08-28 09:15:59 +0100

    Added flow config. Not working fully yet?

--- src/ratpack/static/app/api/jsonApi.js
+++ src/ratpack/static/app/api/jsonApi.js
@@ -1,17 +1,18 @@
 import superagent from 'superagent';
+import logger from '../logger';
 
 export function postJson(url: string): () => void {
   return function (payload: string) {
     superagent
-        .post(url)
-        .send({json: payload})
-        .set('Accept', 'application/json')
-        .end((err, res) => {
-          if (err || !res.ok) {
-            console.log('Oh no! error');
-          } else {
-            console.log('yay got ' + JSON.stringify(res.body));
-          }
-        });
+      .post(url)
+      .send({json: payload})
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (err || !res.ok) {
+          logger.warn('Oh no! error');
+        } else {
+          logger.info('yay got ' + JSON.stringify(res.body));
+        }
+      });
   };
 }
